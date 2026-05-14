ef6cd2aab9560782a1d232b568d1549af761101c
Cristobal-Figueroa <Cristobal-Figueroa@users.noreply.github.com>
2026-05-14 19:21:46 +0000

    deploy: c398f232315d5813bdd2c947173d6c66e7fa0e70

--- assets/cart-PLOtQrIC.js
+++ assets/cart-PLOtQrIC.js
@@ -1,7 +1,7 @@
-import{g as t}from"./utils-CGsYQjqr.js";function e(){const r=(t("so-cart")||[]).map(c=>s(c));document.querySelector(".product-list").innerHTML=r.join("")}function s(a){return`<li class="cart-card divider">
+import{g as t}from"./utils-CGsYQjqr.js";const e="/-Sleep-Outside/";function s(){const r=(t("so-cart")||[]).map(c=>o(c));document.querySelector(".product-list").innerHTML=r.join("")}function o(a){return`<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
-      src="${a.Image}"
+      src="${e}${a.Image.replace("../","")}"
       alt="${a.Name}"
     />
   </a>
@@ -11,4 +11,4 @@ import{g as t}from"./utils-CGsYQjqr.js";function e(){const r=(t("so-cart")||[]).
   <p class="cart-card__color">${a.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${a.FinalPrice}</p>
-</li>`}e();
+</li>`}s();
